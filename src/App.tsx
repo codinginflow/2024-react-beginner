3591a2803416906b0a18cfcae8ba163e6b1e274b
Coding in Flow <info@codinginflow.com>
2024-02-08 11:29:10 +0100

    Part 7

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 
 function App() {
+  function setTodoCompleted(id: number, completed: boolean) {
+    alert(
+      `Todo with id ${id} is now ${completed ? "completed" : "not completed"}`
+    );
+  }
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {dummyData.map((todo) => (
-            <TodoItem todo={todo} />
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted}
+            />
           ))}
         </div>
       </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,13 +2,19 @@ import { Todo } from "../types/todo";
 
 interface TodoItemProps {
   todo: Todo;
+  onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-export default function TodoItem({ todo }: TodoItemProps) {
+export default function TodoItem({ todo, onCompletedChange }: TodoItemProps) {
   return (
     <div>
       <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
-        <input type="checkbox" className="scale-125" />
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
+          className="scale-125"
+        />
         <span className={todo.completed ? "line-through text-gray-400" : ""}>
           {todo.title}
         </span>
