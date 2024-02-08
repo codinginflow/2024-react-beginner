cbd077835962d073e240fb858ab4b3e683d0860f
Coding in Flow <info@codinginflow.com>
2024-02-08 10:21:32 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="py-10 h-screen">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+    </main>
+  );
+}
+
+export default App;
