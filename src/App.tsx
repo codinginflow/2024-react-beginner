c53c82a753273936009a9b774ec5331064206c99
Coding in Flow <info@codinginflow.com>
2024-02-08 10:34:45 +0100

    End of part 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { dummyData } from "./data/todos";
+
 function App() {
   return (
     <main className="py-10 h-screen">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <p key={todo.id} className="text-lg">
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Like this video 👍🏻",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Subscribe to Coding in Flow 📺",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Leave a nice comment 😊",
+    completed: false,
+  },
+];
