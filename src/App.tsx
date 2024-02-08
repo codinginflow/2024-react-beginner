2b0f20ae4b143b17fb202698a2e2c5461869a06e
Coding in Flow <info@codinginflow.com>
2024-02-08 12:25:17 +0100

    End of part 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AddTodoForm from "./components/AddTodoForm";
 import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 
@@ -11,10 +12,22 @@ function App() {
     );
   }
 
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ]);
+  }
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
         <div className="space-y-2">
           {todos.map((todo) => (
             <TodoItem
---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+interface AddTodoFormProps {
+  onSubmit: (title: string) => void;
+}
+
+export default function AddTodoForm({ onSubmit }: AddTodoFormProps) {
+  const [input, setInput] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!input.trim()) return;
+
+    onSubmit(input);
+    setInput("");
+  }
+
+  return (
+    <form className="flex" onSubmit={handleSubmit}>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="What needs to be done?"
+        className="rounded-s-md grow border border-gray-400 p-2"
+      />
+      <button
+        type="submit"
+        className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
